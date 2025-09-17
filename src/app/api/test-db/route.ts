0ffa4aac4f099048f579e41e2579e35c8cf76532
Shane Hall <shanehall42@gmail.com>
2025-09-16 23:00:04 -0400

    Fix ESLint error: remove unused prisma variable

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
-
 export async function GET() {
   try {
     console.log('Testing database connection...')
